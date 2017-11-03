ee833960e54f3ab863023a257183b057baf9eff4
JacquesMagron <33155575+JacquesMagron@users.noreply.github.com>
2017-11-03 13:48:22 +0100

    Add files via upload

---
+++ MINI PROJET/src/main/Collector.java
@@ -0,0 +1,110 @@
+package main;
+
+import java.util.ArrayList;
+
+public class Collector {
+
+	/**
+	 * Find the row, column coordinates of the best element (biggest or smallest) for the given matrix
+	 * @param matrix : an 2D array of doubles
+	 * @param smallestFirst : a boolean, indicates if the smallest element is the best or not (biggest is then the best)
+	 * @return an array of two integer coordinates, row first and then column
+	 */
+	public static int[] findBest(double[][] matrix, boolean smallestFirst) {
+
+    	// TODO review
+		
+		double min = 1000;  // remplacer par la commande pour - l'infini
+		double max = -1000; // remplacer par la commande pour + l'infini
+		int[] bestCoordinates = new int[2];
+		
+		// choisir si mettre 2 if et 4 for (plus optimisée moins lisible)
+			
+			for (int irow = 0; irow < matrix.length ; ++irow ) {
+				for(int icolumn = 0; icolumn < matrix[0].length; ++icolumn) {
+					
+					// for Distance Based Search
+					if (smallestFirst && (matrix[irow][icolumn] < min) ) {
+						
+						min = matrix[irow][icolumn];
+						bestCoordinates[0] = irow;
+						bestCoordinates[1] = icolumn;
+						
+						}
+					
+					// for Similarity Based Search
+					else if( !smallestFirst && matrix[irow][icolumn] > max) { 
+						
+						max = matrix[irow][icolumn];
+						bestCoordinates[0] = irow;
+						bestCoordinates[1] = icolumn;
+						
+						}
+				}
+			}
+		
+		return bestCoordinates;
+	}
+
+	
+	/**
+	 * Find the row, column coordinate-pairs of the n best (biggest or smallest) elements of the given matrix
+	 * @param n : an integer, the number of best elements we want to find 
+	 * @param matrix : an 2D array of doubles
+	 * @param smallestFirst : a boolean,  indicates if the smallest element is the best or not (biggest is the best)
+	 * @return an array of size n containing row, column-coordinate pairs
+	 */
+	public static int[][] findNBest(int n, double[][] matrix, boolean smallestFirst) {
+
+    	// TODO review
+		
+		int[][] NbestCoordinates = new int[2][n];
+		for (int i = 0 ; i < n ; ++i) {
+			
+			// the coordinates of the i-th best element of the matrix are stored in i-th row of returned matrix.
+			NbestCoordinates[i] = findBest(matrix, smallestFirst);
+			
+			// i-th max term from the matrix is taken to a neutral value to be disregarded for next 'i's.
+			// a la place du 2 c'est in i
+			
+			matrix[ NbestCoordinates[i][0] ][ NbestCoordinates[0][i] ] = 0; //trouver une valeure neutre non nulle
+		}
+		
+		return NbestCoordinates;
+	}
+	
+	
+
+	/**
+	 * BONUS 
+	 * Notice : Bonus points are underpriced ! 
+	 * 
+	 * Sorts all the row, column coordinates based on their pixel value
+	 * Hint : Use recursion !
+	 * @param matrix : an 2D array of doubles
+	 * @return A list of points, each point is an array of length 2.
+	 */
+	public static ArrayList<int[]> quicksortPixelCoordinates(double[][] matrix) {
+
+		// TODO implement me correctly for "underpriced" bonus!
+		return new ArrayList<int[]>();
+	}
+
+	
+	/**
+	 * BONUS
+	 * Notice : Bonus points are underpriced !
+	 * 
+	 * Use a quick sort to find the row, column coordinate-pairs of the n best (biggest or smallest) elements of the given matrix
+	 * Hint : return the n first or n last elements of a sorted ArrayList  
+	 * @param n : an integer, the number of best elements we want to find 
+	 * @param matrix : an 2D array of doubles
+	 * @param smallestFirst : a boolean, indicate if the smallest element is the best or not (biggest is the best)
+	 * @return an array of size n containing row, column-coordinate pairs
+	 */
+	public static int[][] findNBestQuickSort(int n, double[][] matrix, boolean smallestFirst) {
+
+    	// TODO implement me correctly for underpriced bonus!
+		return new int[][]{};
+	}
+}
---
+++ MINI PROJET/src/main/SimilarityBasedSearch.java
@@ -0,0 +1,93 @@
+package main;
+
+
+public class SimilarityBasedSearch {
+
+	/**
+	 * Computes the mean value of a gray-scale image given as a 2D array 
+	 * @param image : a 2D double array, the gray-scale Image
+	 * @return a double value between 0 and 255 which is the mean value
+	 */
+	public static double windowMean(double[][] image) {
+		//TODO almost done
+		//assertions
+		
+		double graySum = 0;
+		
+		//sums the gray values of the pixels of the image
+		for (int irow = 0; irow < image.length; ++irow) {
+			for (int icolumn = 0; icolumn < image[0].length; ++icolumn) {
+				graySum += image[irow][icolumn];
+			}
+		}
+		
+		double meanGray = graySum / (image.length * image[0].length);
+		return meanGray; 
+	}
+
+	
+	/**
+	 * Computes the Normalized Cross Correlation of a gray-scale pattern if positioned
+	 * at the provided row, column-coordinate in a gray-scale image
+	 * @param row : a integer, the row-coordinate of the upper left corner of the pattern in the image.
+	 * @param column : a integer, the column-coordinate of the upper left corner of the pattern in the image.
+	 * @param pattern : an 2D array of doubles, the gray-scale pattern to find
+	 * @param image : an 2D array of double, the gray-scale image where to look for the pattern
+	 * @return a double, the Normalized Cross Correlation value at position (row, col) between the pattern and the part of
+	 * the base image that is covered by the pattern, if the pattern is shifted by x and y.
+	 * should return -1 if the denominator is 0
+	 */
+	public static double normalizedCrossCorrelation(int row, int col, double[][] pattern, double[][] image) {
+		
+		// TODO almost done
+		// assertions!
+		// vérifier VARIANCE et ECART TYPE les carrés et tout et tout...
+		double NCC = 0; //Normalized Cross Correlation
+		
+		double imageMean = windowMean(image); // image mean gray value
+		double imageVariance = 0; // sum of the difference the image pixels' gray value and the image's mean gray value
+		
+		double patternMean = windowMean(pattern); // pattern mean gray value
+		double patternVariance = 0; // sum of the difference the pattern pixels' gray value and the image's mean gray value
+				
+		double covarianceSum = 0; // sum of the products of the given pixel's imageVariance and patternVariance
+		
+		// calculates imageVariance, patternVariance, CovarianceSum 
+		for (int irow = 0; irow < pattern.length ; ++irow) {
+			for (int icolumn = 0 ; icolumn < pattern[0].length ; ++ icolumn) {
+				imageVariance += image[irow + row][icolumn + col] - imageMean;
+				patternVariance += pattern[irow][icolumn] - patternMean;	
+				covarianceSum += (image[irow + row][icolumn + col] - imageMean) * (pattern[irow][icolumn] - patternMean);
+				}
+		}
+		
+		double ecartType = (Math.sqrt((imageVariance*imageVariance)*(patternVariance * patternVariance)));
+		if(ecartType == 0) { return -1;}
+		else { NCC = covarianceSum / (Math.sqrt((imageVariance*imageVariance)*(patternVariance * patternVariance))); 
+		return NCC; }
+	}
+
+	
+	/**
+	 * Compute the similarityMatrix between a gray-scale image and a gray-scale pattern
+	 * @param pattern : an 2D array of doubles, the gray-scale pattern to find
+	 * @param image : an 2D array of doubles, the gray-scale image where to look for the pattern
+	 * @return a 2D array of doubles, containing for each pixel of a original gray-scale image, 
+	 * the similarity (normalized cross-correlation) between the image's window and the pattern
+	 * placed over this pixel (upper-left corner)
+	 */
+	public static double[][] similarityMatrix(double[][] pattern, double[][] image) {
+		
+		// TODO almost done
+		// assertions
+		double[][] similarityMatrix = new double[image.length - pattern.length][image[0].length - pattern[0].length];
+		
+		for (int irow = 0; irow < similarityMatrix.length ; ++irow) {
+			for (int icolumn = 0; icolumn < similarityMatrix[0].length ; ++icolumn) {
+				similarityMatrix[irow][icolumn] = normalizedCrossCorrelation(irow, icolumn, pattern, image);
+			}
+		}
+		return similarityMatrix;
+	}
+
+}
